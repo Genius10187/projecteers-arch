7e975c7b1ea8ab6d06a0c0626903739e4af38beb
JoshH <hilbertjosh1@gmail.com>
2018-02-06 19:37:00 -0500

    Added comments for the physics class

--- src/main/java/edu/woodson/Math/Physics.java
+++ src/main/java/edu/woodson/Math/Physics.java
@@ -3,12 +3,21 @@ package edu.woodson.Math;
 public class Physics {
     private static final double GRAVIATAIONAL_CONSTATNT = 0.0000000000674;
 
-    public static double getGravity(double m1, double m2, double r) {
-        double gravity = GRAVIATAIONAL_CONSTATNT * (m1 * m2) / Math.pow(r, 2);
-
-        return gravity;
+    /**
+     *
+     * @param m1 - the first mass
+     * @param m2 - the second mass
+     * @param r - the distance between the centers of the masses
+     * @return the gravitiaional force in NEWTONS
+     */
+    public static double getGravitationalForce(double m1, double m2, double r) {
+        return GRAVIATAIONAL_CONSTATNT * (m1 * m2) / Math.pow(r, 2);
     }
 
+    /**
+     *
+     * @return the Gravitational Constant of 6.674E-11 Newton
+     */
     public static double getGRAVIATAIONAL_CONSTATNT() {
         return GRAVIATAIONAL_CONSTATNT;
     }
